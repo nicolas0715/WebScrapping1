abd22fd959af3ffa03dbfff816c3989f99750f3a
Otero <oteronicolas3@gmail.com>
2023-06-17 09:53:43 -0300

    p2

--- pusheo.py
+++ pusheo.py
@@ -1,26 +1,25 @@
 import os
 from git import Repo
 
+# Ruta local al repositorio
+repo_path = 'C:/Users/otero/Desktop/Pokio/Programacion/Proyectos/Proyectos_varios/Web_Scrapping4'
 
-def pushear():
-    # Ruta local al repositorio
-    repo_path = 'C:/Users/otero/Desktop/Pokio/Programacion/Proyectos/Proyectos_varios/Web_Scrapping4'
-    # Pero para ejecutarlo con Actions, necesito la ruta local
-    ruta_raiz = os.getcwd()
+# Pero para ejecutarlo con Actions, necesito la ruta local
+ruta_raiz = os.getcwd()
 
-    # Inicializar el repositorio
-    repo = Repo(ruta_raiz)
+# Inicializar el repositorio
+repo = Repo(ruta_raiz)
 
-    # Agregar los archivos al repositorio (opcional)
-    repo.index.add('productos.csv')
+# Agregar los archivos al repositorio (opcional)
+repo.index.add('productos.csv')
 
-    # Commit con un mensaje
-    repo.index.commit('Commit desde script')
-    
-    token = os.getenv('GITHUB_TOKEN')
+# Commit con un mensaje
+repo.index.commit('Commit desde script')
 
-    # Realizar el push al repositorio remoto
-    origin = repo.remote('origin')
-    origin_url = origin.url.replace('https://', f'https://token:{token}@') 
-    origin.set_url(origin_url)
-    origin.push()
+token = os.getenv('GITHUB_TOKEN')
+
+# Realizar el push al repositorio remoto
+origin = repo.remote('origin')
+origin_url = origin.url.replace('https://', f'https://token:{token}@') 
+origin.set_url(origin_url)
+origin.push()
