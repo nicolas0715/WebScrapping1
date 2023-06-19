b670d7fab67f8163c5d68e4d106914558b1a7a8b
Otero <oteronicolas3@gmail.com>
2023-06-19 19:38:59 -0300

    Prueba 09

--- pusheo.py
+++ pusheo.py
@@ -2,31 +2,27 @@ import os
 from git import Repo
 
 def push():
-    try:
-        # Ruta local al repositorio
-        #repo_path = 'C:/Users/otero/Desktop/Pokio/Programacion/Proyectos/Proyectos_varios/Web_Scrapping1'
-
-        # Pero para ejecutarlo con Actions, necesito la ruta local
-        ruta_raiz = os.getcwd()
-
-        # Inicializar el repositorio
-        repo = Repo(ruta_raiz)
-
-        # Agregar los archivos al repositorio (opcional)
-        repo.index.add('productos.csv')
-
-        # Commit con un mensaje
-        repo.index.commit('Commit desde script')
-
-        token = os.getenv('NICO_TOKEN2')
-        print(token)
-
-        # Realizar el push al repositorio remoto
-        origin = repo.remote('origin')
-        origin_url = origin.url.replace('https://', f'https://token:{token}@') 
-        origin.set_url(origin_url)
-        origin.push()
-
-    except Exception as e:
-        print('El token es: ' + str(token))
-        print('Error al realizar el push:', str(e)) 
+    # Token
+    token = os.getenv('NICO_TOKEN')
+    print(token)
+    
+    # Ruta local al repositorio
+    #repo_path = 'C:/Users/otero/Desktop/Pokio/Programacion/Proyectos/Proyectos_varios/Web_Scrapping1'
+    
+    # Pero para ejecutarlo con Actions, necesito la ruta local
+    ruta_raiz = os.getcwd()
+    
+    # Inicializar el repositorio
+    repo = Repo(ruta_raiz)
+    
+    # Agregar los archivos al repositorio (opcional)
+    repo.index.add('productos.csv')
+    
+    # Commit con un mensaje
+    repo.index.commit('Commit desde script')
+    
+    # Realizar el push al repositorio remoto
+    origin = repo.remote('origin')
+    origin_url = origin.url.replace('https://', f'https://token:{token}@') 
+    origin.set_url(origin_url)
+    origin.push()
