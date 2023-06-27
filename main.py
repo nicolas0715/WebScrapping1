9127ca891df494127db0d214421f6f04d15362f3
Otero <oteronicolas3@gmail.com>
2023-06-26 23:54:34 -0300

    Prueba 10

--- main.py
+++ main.py
@@ -260,24 +260,24 @@ print('='*80)
 
 # Falta arreglar para que se impriman los nombres de las listas
 html_mail1 = f'''
-<h3 style='margin:5px auto;'>Estos son los productos que aumentaros:</h3>
+<h3 style='margin:10px auto; color:green; font-size: 25px;'>Estos son los productos que aumentaros:</h3>
 <div style="display: flex; justify-content: center;"> 
-    <table style="margin: 10px 20px;">
-        <th colspan="2">Precio Anterior</th>
+    <table style="margin: 10px 20px; background-color: gray; border: 2px solid black; border-radius: 10px; padding: 0px 8px;">
+        <th colspan="2" style="padding:10px 0px; border-bottom: 2px solid black; font-size: 20px;">Precio Anterior</th>
 '''
 for producto, valores in productos_html.items():
-    html_mail1 += f'<tr><td>{valores[0]}</td>'
-    html_mail1 += f'<td>{valores[1]}</td></tr>'
+    html_mail1 += f'<tr><td style="padding:5px 10px 5px 0px; border-right: 2px solid black;">{valores[0]}</td>'
+    html_mail1 += f'<td style="text-align:center; padding: 0px 8px;">{valores[1]}</td></tr>'
 
 html_mail1 += '''
 </table>
-<table style="margin: 10px 20px;">
-    <th colspan="2">Precio Nuevo</th>
+<table style="margin: 10px 20px; background-color: gray; border: 2px solid black; border-radius: 10px; padding: 0px 8px;">
+    <th colspan="2" style="padding:10px 0px; border-bottom: 2px solid black; font-size: 20px;">Precio Nuevo</th>
 '''
 
 for producto, valores in productos_html.items():
-    html_mail1 += f'<tr><td>{valores[0]}</td>'
-    html_mail1 += f'<td>{valores[2]}</td></tr>'
+    html_mail1 += f'<tr><td style="padding:5px 10px 5px 0px; border-right: 2px solid black;">{valores[0]}</td>'
+    html_mail1 += f'<td style="text-align:center; padding: 0px 8px;">{valores[2]}</td></tr>'
 
 html_mail1 += '''
 </table>
