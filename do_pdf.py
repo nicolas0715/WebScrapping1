8a07df5cf796dc518ffb55a54d0b9795a76ebf76
Otero <oteronicolas3@gmail.com>
2023-08-09 17:24:10 -0300

    Editado el tamano de la fuente del pdf

--- do_pdf.py
+++ do_pdf.py
@@ -23,7 +23,7 @@ def pdf_gen():
         ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
         ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
         ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-        ('FONTSIZE', (0, 0), (-1, 0), 12),
+        ('FONTSIZE', (0, 0), (-1, 0), 10),
         ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
         ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
         ('GRID', (0, 0), (-1, -1), 1, colors.black),
