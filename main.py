e2ea2d524c328f42b6be49a13087ecc288f8e225
Otero <oteronicolas3@gmail.com>
2023-08-09 19:18:52 -0300

    Prueba con aumento

--- main.py
+++ main.py
@@ -268,7 +268,7 @@ for producto, valores in productos_html.items():
     html_mail1 += f'<tr><td style="text-align:center;padding:5px 10px 5px 0px; border-right: 2px solid black;">{valores[0]}</td>'
     html_mail1 += f'<td style="text-align:center;padding:5px 10px 5px 0px; border-right: 2px solid black;">{valores[1]}</td>'
     html_mail1 += f'<td style="text-align:center;padding:5px 10px 5px 0px; border-right: 2px solid black;">{valores[2]}</td>'
-    variacion = round((100 * float(valores[2])) / float(valores[1]), 2) - 100
+    variacion = round((100 * float(valores[2])) / float(valores[1]) - 100, 2)
     html_mail1 += f'<td style="text-align:center;padding:5px 10px 5px 0px">%{variacion}</td></tr>'
 
 html_mail1 += '''
