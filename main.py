cc9e7743d0ba7732fbc0083eee46f24948557108
Otero <oteronicolas3@gmail.com>
2023-08-24 08:51:06 -0300

    Prueba General 12.

--- main.py
+++ main.py
@@ -27,11 +27,11 @@ with open(archivo_csv, 'r') as archivo:
 #--------------------------------------------------------------------------------------------------------------------#
 
 # Configurar el servicio y el controlador de Selenium
-#desired_version = "114.0.5735.90"
-desired_version = "115.0.5790"  
-service = ChromeService(ChromeDriverManager(version=desired_version).install())
-chrome_options = Options()
-chrome_options.add_argument("--headless")
+#desired_version = "115.0.5790"  
+#service = ChromeService(ChromeDriverManager(version=desired_version).install())
+service = ChromeService()
+chrome_options = webdriver.ChromeOptions()
+#chrome_options.add_argument("--headless")
 driver = webdriver.Chrome(service=service, options=chrome_options)
 
 # Acceder a la página de inicio de sesión
